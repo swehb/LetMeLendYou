bc7603e1533bd3017be662769574927e08c6ccf4
Hannah Barath <hannahbarath@gmail.com>
2023-03-14 16:25:36 -0700

    html template creation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from flask import Flask
-from flask import url_for
-from flask import render_template, request
-
-
+from flask import url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -26,19 +23,19 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/new-user")
+@app.route("/new_user")
 def new_user():
     # username field - verify that it is not taken in database
     # password field
     # password verification field
     # email address - optional but needed for PW resets and reminders
-    return "<p> new user creation page </p>"
+    return render_template("new_user.html")
 
 @app.route("/forgot_password")
 def forgot_password():
     # enter email or username
     # if email does not exist in db, do nothing, else send password
-    return "<p> you forgot your password </p>"
+    return render_template("forgot_password.html")
 
 @app.route("/home")
 def home():
@@ -59,7 +56,7 @@ def add_entry():
 #api routes
 
 @app.route("/api/login", methods=["GET", "POST"])
-def index():
+def login_verification():
     access_granted = False
     username = request.form.get("username", "unknown")
     password = request.form.get("password", "unknownpass")
@@ -67,21 +64,29 @@ def index():
     print("hello world login")
     #check if user is OK from the database
     if access_granted == False:
-        print("access granted")
-        return("access granted")
-        # return main user page
-    else:
         print("access not granted")
         return("access not granted")
+        # return main user page
+    else:
+        print("access granted")
+        return("access granted")
         # return login page
     return("success")
 
 
+@app.route("/api/new_user", methods=["GET", "POST"])
+def new_user_creation():
+    # verify if username is available
+    # verify if PW meets qualifications
+        # enter password again, must match
+    # creates the new user and password combination
+    # verify correct email format (xx@xx.xx)
+    # redirect to login page - but autofill the username
+    return "<p> new user placeholder </p>"
 
-@app.route("/hello <name>")
-def hello(name):
-    return f"Hello, {name}!"
 
 
-if __name__=="__main__":
-    app.run(debug=True)
+
+# NOTE - not sure what this is for. do we need it? 
+# if __name__=="__main__":
+#     app.run(debug=True)
